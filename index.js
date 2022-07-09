8afe2e8d364cdf699c63ac3954c0d1dc29025702
Joe Scott <jbs198585@gmail.com>
2022-07-09 06:28:49 -0400

    Auth Demo: Registering

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/loginDemo', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MONGO CONNECTION OPEN!!!")
   })
@@ -17,12 +18,23 @@ app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('THIS IS THE HOME PAGE')
+})
+
 app.get('/register', (req, res) => {
   res.render('register')
 })
 
 app.post('/register', async (req, res) => {
-  res.send(req.body)
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash
+  })
+  await user.save();
+  res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {
